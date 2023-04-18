150f3f3ce4c3469f284e955a4423540921f444a3
Mohamedsamy123 <66519356+Mohamedsamy123@users.noreply.github.com>
2023-04-18 22:27:24 +0200

    Update applicationRoute.js

--- routes/applicationRoute.js
+++ routes/applicationRoute.js
@@ -3,8 +3,26 @@ const router = express.Router();
 const {
   applyForJob,
   getAllApps,
+  getSingleJobApps,
+  getApp,
+  deleteApp,
+  editAppStatus,
 } = require("../controllers/applicationController");
+const { authenticate } = require("../middleware/authentication");
+const { authorize } = require("../middleware/authorization");
 
-router.route("/").post(applyForJob).get(getAllApps);
+router
+  .route("/")
+  .post(authenticate, authorize("candidate"), applyForJob)
+  .get(authenticate, authorize("candidate"), getAllApps);
+router
+  .route("/:id")
+  .get(authenticate, authorize("candidate"), getApp)
+  .delete(authenticate, authorize("candidate"), deleteApp)
+  .patch(authenticate, authorize("hr"), editAppStatus);
+
+router
+  .route("/:id/applications")
+  .get(authenticate, authorize("hr"), getSingleJobApps);
 
 module.exports = router;
