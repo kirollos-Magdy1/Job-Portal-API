7f5b8e4a350124cd3fdc3914f446dfa699e9e8be
Hammam <ahmed.moo.hamam@gmail.com>
2023-04-18 20:17:16 +0200

    add edit and delete

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -27,8 +27,43 @@ const getSingleJob = async (req, res) => {
   res.status(StatusCodes.OK).json({ job });
 };
 
+const editJob = async (req, res) => {
+  const job = await Job.findByIdAndUpdate(
+    { _id: req.params.id, hr: req.user.id },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!job) {
+    throw new CustomError.NotFoundError(
+      "No job found with id " + req.params.id
+    );
+  }
+
+  res.status(StatusCodes.OK).json({ job });
+};
+
+const deleteJob = async (req, res) => {
+  const job = await Job.findByIdAndDelete({
+    _id: req.params.id,
+    hr: req.user.id,
+  });
+  if (!job) {
+    throw new CustomError.NotFoundError(
+      "No job found with id " + req.params.id
+    );
+  }
+
+  res.status(StatusCodes.OK).json({ msg: "job delted" });
+};
+
 module.exports = {
   createJob,
   getAllJobs,
   getSingleJob,
+  editJob,
+  deleteJob,
 };
